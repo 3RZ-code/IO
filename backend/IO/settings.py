f5c82d5cc760df279e894d9bb477b7f5aac0311e
Jai8a5 <Jai8a5@protonmail.com>
2025-10-07 10:57:35 +0200

    docker database implementation

--- backend/IO/settings.py
+++ backend/IO/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'mydb',
         'USER': 'myuser',
         'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
