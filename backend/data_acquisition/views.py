97180237752bed6448de600ffd640dd420f93ae8
Jai8a5 <Jai8a5@protonmail.com>
2025-10-09 11:35:49 +0200

    read data from table, issue api

--- backend/data_acquisition/views.py
+++ backend/data_acquisition/views.py
@@ -1,4 +1,15 @@
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from data_acquisition.utils.serializers import DeviceReadingSerializer
+from data_acquisition.utils.device_reading import get_all_readings
 
 def index(request):
     return HttpResponse("Data acquisition module!")
+
+class DeviceReadingList(APIView):
+    def get(self,request):
+        readings = get_all_readings()
+        serializer = DeviceReadingSerializer(readings, many=True)
+        return Response(serializer.data)
+
