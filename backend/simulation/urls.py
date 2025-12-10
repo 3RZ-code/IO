14bc8e4475d77d858c11f21cd3e1f92d480b044b
Krzysztof Miller <251591@edu.p.lodz.pl>
2025-12-10 04:36:42 +0100

    urls add simulation start

--- backend/simulation/urls.py
+++ backend/simulation/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_all_devices
+from .views import get_all_devices, trigger_simulation
 
 urlpatterns = [
     path("devices/", get_all_devices),  # GET /simulation/devices/
+    path("run/", trigger_simulation),  # Uruchomienie symulacji (Live lub Demo)
 ]
